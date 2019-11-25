26991bf410e798d7ab9e12b66d4b4427040bbc62
didier-tp <didier@d-d-efrance.fr>
2019-11-25 12:15:48 +0100

    debut jaxb2

--- tpJavaXml/src/main/java/fr/afcepf/al34/tp/data/Produit.java
+++ tpJavaXml/src/main/java/fr/afcepf/al34/tp/data/Produit.java
@@ -1,8 +1,27 @@
 package fr.afcepf.al34.tp.data;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+//l'api JAXB2 est utilisé en interne par l'api JAX-WS (Web service Soap)
+
+//annotations de JAXB2 (package "javax.xml.bind..." et @Xml...() 
+//JAXB2 est intégré dans la JVM à partir de java 6 (==> java 1.8 dans pom.xml)
+@XmlRootElement(name = "produit", namespace = "http://www.example.org/produit")
+@XmlAccessorType(XmlAccessType.FIELD) //pour demander à analyser les @Xml...()
+                                      //au dessus des FIELD (souvent private)
 public class Produit {
+	
+	@XmlAttribute(name = "ref")
 	private String ref;
+	
+	@XmlElement(name="label")
 	private String label;
+	
+	@XmlElement(name="prix")
 	private Double prix;
 	
 	public Produit() {
