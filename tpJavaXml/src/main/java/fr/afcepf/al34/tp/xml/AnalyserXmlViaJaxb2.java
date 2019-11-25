e7cc049ab09d93d70148a505a401be477099fd7d
didier-tp <didier@d-d-efrance.fr>
2019-11-25 14:18:27 +0100

    e

--- tpJavaXml/src/main/java/fr/afcepf/al34/tp/xml/AnalyserXmlViaJaxb2.java
+++ tpJavaXml/src/main/java/fr/afcepf/al34/tp/xml/AnalyserXmlViaJaxb2.java
@@ -20,9 +20,18 @@ public class AnalyserXmlViaJaxb2 {
 					JAXBContext.newInstance(Produit.class);
 			Unmarshaller um = jctx.createUnmarshaller(); 
 			// pour remonter des objets en mémoire // suite à la lecture d'un flux xml 
-			// Marshaller m = jctx.createMarshaller();// pour ecrire
+			
 			Produit p = (Produit) um.unmarshal( new File( fileName ) );
 			System.out.println(p);
+			
+			Marshaller m = jctx.createMarshaller();// pour ecrire
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			
+			p.setLabel(p.getLabel().toUpperCase());//modif en mémoire
+			
+			//écriture dans nouveau fichier f3:
+			m.marshal(p, new File("src/main/resources/f3.xml"));//+Refresh eclipse
+			
 		} catch (Exception e) {
 			
 			e.printStackTrace();
