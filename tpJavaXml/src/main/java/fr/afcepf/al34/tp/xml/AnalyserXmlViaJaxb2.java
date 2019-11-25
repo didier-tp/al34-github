ce53a9478849f6bbf343bcf205d27624f783c215
didier-tp <didier@d-d-efrance.fr>
2019-11-25 14:37:29 +0100

    avec commande

--- tpJavaXml/src/main/java/fr/afcepf/al34/tp/xml/AnalyserXmlViaJaxb2.java
+++ tpJavaXml/src/main/java/fr/afcepf/al34/tp/xml/AnalyserXmlViaJaxb2.java
@@ -6,12 +6,38 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
+import fr.afcepf.al34.tp.data.Commande;
 import fr.afcepf.al34.tp.data.Produit;
 
 public class AnalyserXmlViaJaxb2 {
 
 	public static void main(String[] args) {
 		analyserXmlViaJaxb2("src/main/resources/produit.xml");
+		analyserCommandeXmlViaJaxb2("src/main/resources/commande.xml");
+	}
+	
+	private static void analyserCommandeXmlViaJaxb2(String fileName) {
+		try {
+			JAXBContext jctx =     
+					JAXBContext.newInstance(Commande.class);
+			Unmarshaller um = jctx.createUnmarshaller(); 
+			// pour remonter des objets en mémoire // suite à la lecture d'un flux xml 
+			
+			Commande c = (Commande) um.unmarshal( new File( fileName ) );
+			System.out.println(c);
+			
+			Marshaller m = jctx.createMarshaller();// pour ecrire
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			
+			c.setAdresse(c.getAdresse().toUpperCase());//modif en mémoire
+			
+			//écriture dans nouveau fichier f4:
+			m.marshal(c, new File("src/main/resources/f4.xml"));//+Refresh eclipse
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
 	}
 
 	private static void analyserXmlViaJaxb2(String fileName) {
