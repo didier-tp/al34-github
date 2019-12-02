c01b8f9a7e4d57895e989edb6a934e8e91d7b98a
didier-tp <didier@d-d-efrance.fr>
2019-12-02 09:24:14 +0100

    hop hop

---
+++ myPatterns/src/main/java/fr/afcepf/al34/calculateur/CalculateurDecorateur.java
@@ -0,0 +1,34 @@
+package fr.afcepf.al34.calculateur;
+
+public abstract class CalculateurDecorateur implements Calculateur{
+	protected Calculateur calculateur; //calculateur de base à décorer
+
+	@Override
+	public double addition(double a, double b) {
+		return calculateur.addition(a, b);
+	}
+
+	@Override
+	public double multiplication(double a, double b) {
+		return calculateur.multiplication(a, b);
+	}
+	
+	public CalculateurDecorateur() {
+		this(null);
+	}
+
+	public CalculateurDecorateur(Calculateur calculateur) {
+		super();
+		this.calculateur = calculateur;
+	}
+
+	public final Calculateur getCalculateur() {
+		return calculateur;
+	}
+
+	public final void setCalculateur(Calculateur calculateur) {
+		this.calculateur = calculateur;
+	}
+	
+	
+}
