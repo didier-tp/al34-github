922b951e060bfe44853c5307442336a838aaa8a0
didier-tp <didier@d-d-efrance.fr>
2019-12-09 14:39:37 +0100

    avec DriverManagerDataSource xml

--- demo/src/main/java/fr/afcepf/al34/demo/dao/BlagueDaoImpl.java
+++ demo/src/main/java/fr/afcepf/al34/demo/dao/BlagueDaoImpl.java
@@ -1,5 +1,8 @@
 package fr.afcepf.al34.demo.dao;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import fr.afcepf.al34.demo.entity.Blague;
@@ -7,11 +10,15 @@ import fr.afcepf.al34.demo.entity.Blague;
 //@Component
 @Repository //id par defaut = blagueDaoImpl
 public class BlagueDaoImpl implements BlagueDao {
+	
+	@Autowired
+	private DataSource dataSource;
 
 	@Override
 	public Blague findById(Long id) {
 		// v1 : simulation sans base de données
-		return new Blague(id ,"blague_" + id , "texte de la blague");
+		String texte="texte de la blague " + dataSource.toString() ;
+		return new Blague(id ,"blague_" + id , texte);
 	}
 
 }
