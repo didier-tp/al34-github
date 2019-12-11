1dd4a2cb67783f84dbb120b16bfebc6d5100f006
didier-tp <didier@d-d-efrance.fr>
2019-12-11 12:25:57 +0100

    //@Scope("singleton") par defaut

--- springBootJsf/src/main/java/fr/afcepf/al34/tp/init/InitDataSet.java
+++ springBootJsf/src/main/java/fr/afcepf/al34/tp/init/InitDataSet.java
@@ -4,6 +4,7 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import fr.afcepf.al34.tp.entity.Compte;
@@ -14,6 +15,7 @@ import fr.afcepf.al34.tp.service.CompteService;
 @Profile("initData") //code ci-dessus activé que si le profile "initData" est choisi (parmis d'autres)
                      //au démarrage d'un test ou de l'application
 @Component
+//@Scope("singleton") par defaut
 public class InitDataSet {
 	
 	@Autowired
