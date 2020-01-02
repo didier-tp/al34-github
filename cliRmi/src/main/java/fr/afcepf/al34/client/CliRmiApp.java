6b41fe80650d243b0d95fd29c5c6fea7b66897ea
didier-tp <didier@d-d-efrance.fr>
2020-01-02 11:20:15 +0100

    cliRmi

--- cliRmi/src/main/java/fr/afcepf/al34/client/CliRmiApp.java
+++ cliRmi/src/main/java/fr/afcepf/al34/client/CliRmiApp.java
@@ -11,10 +11,14 @@ public class CliRmiApp {
 	public static void main(String[] args) {
 		try {
 			String machineServeur = "localhost"; //ou "192.168.xx.yy"
+			//192.168.102.183 ,  102.38 , ....
+			//connection au serveur de noms logiques "RmiRegistry":
 			Registry registry = LocateRegistry.getRegistry(machineServeur,1099);
 			String nomLogiqueCalculateur= "calculateurTva";
+			//connection à l'objet distant RMI via son nom logique
 			ICalculTva proxyCalculateurTva = 
 					(ICalculTva) registry.lookup(nomLogiqueCalculateur);
+			//appels de méthodes à distance (via le réseau):
 			double tva= proxyCalculateurTva.tva(200.0, 20.0);
 			String auteur = proxyCalculateurTva.getAuteur();
 			System.out.println("tva="+tva);
