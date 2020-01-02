0457dbddee6c7c1f72d46b46cdc8b37af218471d
didier-tp <didier@d-d-efrance.fr>
2020-01-02 15:19:24 +0100

    suite soap

--- servEjb/src/main/java/fr/afcepf/al34/serveur/CalculTvaImpl.java
+++ servEjb/src/main/java/fr/afcepf/al34/serveur/CalculTvaImpl.java
@@ -2,15 +2,20 @@ package fr.afcepf.al34.serveur;
 
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.jws.WebService;
 
 import fr.afcepf.al34.dto.ResCalculTva;
 import fr.afcepf.al34.itf.ICalculTva;
 
 //Surveiller les messages de la console Jboss pour connaître les noms
 //des EJB "@Remote" enregistrés dans la partie "noms logiques JNDI" de JBOSS
+//et aussi pour connaître l'URL de la description WSDL du WS SOAP
+//à tester via soap-ui .
 
 @Stateless
-@Remote //ici ou bien sur l'interface
+@Remote //pour access rmi , ici ou bien sur l'interface
+@WebService(endpointInterface = "fr.afcepf.al34.itf.ICalculTva") //pour access soap
+//@Remote peut cohabiter avec @WebService
 public class CalculTvaImpl  implements ICalculTva {
 
 	protected CalculTvaImpl() {
