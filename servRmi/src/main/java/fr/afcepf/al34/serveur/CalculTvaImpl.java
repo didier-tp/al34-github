501677eb5d50963cf5d159ae50f9e74138f1d9e7
didier-tp <didier@d-d-efrance.fr>
2020-01-02 10:52:58 +0100

    debut rmi

---
+++ servRmi/src/main/java/fr/afcepf/al34/serveur/CalculTvaImpl.java
@@ -0,0 +1,27 @@
+package fr.afcepf.al34.serveur;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import fr.afcepf.al34.itf.ICalulTva;
+
+public class CalculTvaImpl extends UnicastRemoteObject implements ICalulTva {
+
+	protected CalculTvaImpl() throws RemoteException {
+		super(); //appel indispensable du constructeur de UnicastRemoteObject
+		//pour que l'objet soit accessible à distance via RMI
+	}
+
+	public double tva(double ht, double tauxTva) throws RemoteException {
+		return ht * tauxTva/100;
+	}
+
+	public double ttc(double ht, double tauxTva) throws RemoteException {
+		return ht * (1 + tauxTva/100);
+	}
+
+	public String getAuteur() throws RemoteException {
+		return "didier / formateur fou";
+	}
+
+}
