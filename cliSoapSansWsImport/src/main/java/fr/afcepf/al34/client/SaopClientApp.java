dff3c529722e06bc4e17f8895c9c34769d180295
didier-tp <didier@d-d-efrance.fr>
2020-01-03 16:44:42 +0100

    avec business deleguate

--- cliSoapSansWsImport/src/main/java/fr/afcepf/al34/client/SaopClientApp.java
+++ cliSoapSansWsImport/src/main/java/fr/afcepf/al34/client/SaopClientApp.java
@@ -1,38 +1,16 @@
 package fr.afcepf.al34.client;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
-
 import fr.afcepf.al34.ws.service.ICalculTva;
 
 public class SaopClientApp {
 
 	public static void main(String[] args) {
-		// appeler un WS Soap codé en java (dans le même langage)
-		// on peut se passer de wsimport.
-		//NB: ce code ne fonctionne que si l'interface ICalculTva comporte l'annotation @WebService
-
-		QName SERVICE_NAME = new QName("http://service.ws.al34.afcepf.fr/", "CalculTvaImplService");
-		QName PORT_NAME = new QName("http://service.ws.al34.afcepf.fr/", "CalculTvaImplPort");
-		// en précisant une URL WSDL connue et accessible
 		
-		String wdlUrl =
-		"http://localhost:8080/springBootWs/service/calculTva?wsdl";
-		URL wsdlDocumentLocation=null;
-		try {wsdlDocumentLocation = new URL(wdlUrl);
-		} catch (MalformedURLException e) { e.printStackTrace();}
+		ICalculTva calculateurTva = new CalculTvaDeleguate();
 		
-		//avec import javax.xml.ws.Service;
-		Service service = Service.create(wsdlDocumentLocation, SERVICE_NAME);
-		ICalculTva caculateurWSProxy = (ICalculTva)
-		                service.getPort(PORT_NAME, ICalculTva.class);
-		
-		double resTva = caculateurWSProxy.ttc(200.0, 20.0);
+		double resTva = calculateurTva.ttc(200.0, 20.0);
 		System.out.println("resTva="+resTva);
-		System.out.println("auteur="+caculateurWSProxy.getAuteur());
+		System.out.println("Auteur="+calculateurTva.getAuteur());
 	}
 
 }
