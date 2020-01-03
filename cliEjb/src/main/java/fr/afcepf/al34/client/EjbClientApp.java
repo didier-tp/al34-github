0708f1d5f4fa809eef825c806cf27508fb558326
didier-tp <didier@d-d-efrance.fr>
2020-01-03 13:42:01 +0100

    cliEjb ok

--- cliEjb/src/main/java/fr/afcepf/al34/client/EjbClientApp.java
+++ cliEjb/src/main/java/fr/afcepf/al34/client/EjbClientApp.java
@@ -15,24 +15,25 @@ public class EjbClientApp {
 	public static void main(String[] args) {
 		try {
 			String machineServeur = "localhost"; //ou "192.168.xx.yy"
-			//192.168.102.183 ,  102.38 , ....
+			//String machineServeur = "192.168.102.183"; //192.168.102.183 ,  102.38 , ....
 			
 			Properties props = new Properties();//java.util
 			//Context et InitialContext de javax.naming (JNDI)
 			
-			props.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+			//props.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
 			
 			
-			props.put(Context.INITIAL_CONTEXT_FACTORY,/*WildFlyInitialContextFactory.class.getName()*/
-		    			/*"org.jboss.naming.remote.client.InitialContextFactory"*/
-					    "org.wildfly.naming.client.WildFlyInitialContextFactory");
-			props.put(Context.PROVIDER_URL, "remote+http://localhost:8080");
+			props.put(Context.INITIAL_CONTEXT_FACTORY,
+		    			"org.jboss.naming.remote.client.InitialContextFactory"
+					/*"org.wildfly.naming.client.WildFlyInitialContextFactory"*/);
+					    
+			props.put(Context.PROVIDER_URL, "http-remoting://"+machineServeur+":8080");
 					// remote://localhost:4447 for Jboss7.1 , http-remoting://localhost:8080 for wildfly 8,9
-			props.put(Context.SECURITY_PRINCIPAL, "guest"); // username : "admin" , "guest" , "..."
-			props.put(Context.SECURITY_CREDENTIALS, "guest007"); //password : "pwd", "guest007"
+			//props.put(Context.SECURITY_PRINCIPAL, "guest"); // username : "admin" , "guest" , "..."
+			//props.put(Context.SECURITY_CREDENTIALS, "guest007"); //password : "pwd", "guest007"
 					//avec utilisateur ajouté via la commande JBOSS_7_HOME/bin/add-user
 					//et roles associés admin,guest,.... sur partie "ApplicationRealm" .
-			props.put("jboss.naming.client.ejb.context", true); //indispensable pour accès @Remote
+			//props.put("jboss.naming.client.ejb.context", true); //quelquefois indispensable pour accès @Remote
 			Context jndiContext = new InitialContext(props);
 			
 			//NB: ce nom logique (respectant la norme JEE6) s'affiche dans la console jboss
@@ -47,7 +48,7 @@ public class EjbClientApp {
 			ResCalculTva resCalculTva = proxyCalculateurTva.tvaEtTtc(200.0, 20.0);
 			System.out.println("ttc="+resCalculTva.getTtc());
 			System.out.println("tva="+tva);
-			System.out.println("auteur="+auteur);
+			System.out.println("auteur (ejb remote)="+auteur);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
