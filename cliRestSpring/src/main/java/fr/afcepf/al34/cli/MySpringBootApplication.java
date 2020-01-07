0ea7a049e59d2d70a92cae1bbb84bc0116a38e55
didier-tp <didier@d-d-efrance.fr>
2020-01-07 15:18:01 +0100

    business delegate spring

--- cliRestSpring/src/main/java/fr/afcepf/al34/cli/MySpringBootApplication.java
+++ cliRestSpring/src/main/java/fr/afcepf/al34/cli/MySpringBootApplication.java
@@ -3,10 +3,11 @@ package fr.afcepf.al34.cli;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import fr.afcepf.al34.cli.dto.DeviseDto;
-import fr.afcepf.al34.cli.service.DeviseServiceDelegate;
+import fr.afcepf.al34.cli.service.IDeviseService;
 
 
 //NB: @SpringBootApplication est un équivalent
@@ -22,13 +23,21 @@ public static void main(String[] args) {
 	//app.setAdditionalProfiles("initData","swagger");
 	ConfigurableApplicationContext context = app.run(args);
 	System.out.println("http://localhost:8282/cliRestSpring");
-	testAppelWsRest();
+	testAppelWsRest(context);
 }
 
-private static void testAppelWsRest() {
+private static void testAppelWsRest(ApplicationContext contextSpring) {
+	/*
+	// variante 1 avec DeviseServiceDelegate sans @Service (pas vu comme composant Spring)
 	DeviseServiceDelegate deviseServiceDelegate = new DeviseServiceDelegate();
 	DeviseDto deviseEur = deviseServiceDelegate.getDeviseByCode("EUR");
 	System.out.println("deviseEur="+deviseEur);
+	*/
+	
+	//variante 2 avec DeviseServiceDelegate comportant @Service (vu comme composant Spring)
+	IDeviseService deviseServiceDelegate = contextSpring.getBean(IDeviseService.class); //équivalent de @Autowired
+	DeviseDto deviseEur = deviseServiceDelegate.getDeviseByCode("EUR");
+	System.out.println("** deviseEur="+deviseEur);
 }
 
 }
