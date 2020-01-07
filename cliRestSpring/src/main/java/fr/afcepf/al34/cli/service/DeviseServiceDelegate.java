e856ad0293f117785217de6886bafa23d835399e
didier-tp <didier@d-d-efrance.fr>
2020-01-07 16:00:08 +0100

    avec @Value

--- cliRestSpring/src/main/java/fr/afcepf/al34/cli/service/DeviseServiceDelegate.java
+++ cliRestSpring/src/main/java/fr/afcepf/al34/cli/service/DeviseServiceDelegate.java
@@ -3,6 +3,7 @@ package fr.afcepf.al34.cli.service;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +13,13 @@ import fr.afcepf.al34.cli.dto.DeviseDto;
 public class DeviseServiceDelegate implements IDeviseService {
 	
 	private RestTemplate restTemplate = new RestTemplate();
-	private String baseUrl="http://localhost:8080/springBootWs/devise-api/public/devise";
+	
+	//private String baseUrl="http://localhost:8080/springBootWs/devise-api/public/devise";
+	
+	@Value("${devise-api.base-url}") //pour récupérer 
+	// devise-api.base-url=http://localhost:8080/springBootWs/devise-api/public/devise
+	// dans application.properties
+	private String baseUrl;
 
 	@Override
 	public DeviseDto getDeviseByCode(String code) {
