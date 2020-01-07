79b80a91c7190f10dc31f42fccf58f758e94a52c
didier-tp <didier@d-d-efrance.fr>
2020-01-07 12:32:38 +0100

    post ajax

--- springBootWs/src/main/resources/META-INF/resources/appelAjax.js
+++ springBootWs/src/main/resources/META-INF/resources/appelAjax.js
@@ -1,13 +1,34 @@
 var divRes ;
+var divMessage ;
 window.onload=function(){
 	var inputChangeMini = document.getElementById("txtChangeMini");
 	var btnGetDevises = document.getElementById("btnGetDevises");
+	var btnPostDevise = document.getElementById("btnPostDevise");
 	divRes = document.getElementById("divRes");
 	
 	btnGetDevises.addEventListener("click" , function (){
 		var changeMini = inputChangeMini.value;
 		makeAjaxGetRequest("./devise-api/public/devise?changeMini="+changeMini ,  cbGererResultat);
 	});
+	
+	btnPostDevise.addEventListener("click" , function (){
+		var nouvelleDevise =  {
+			code : null,
+			name : null,
+			change : null
+		};
+		nouvelleDevise.code = document.getElementById("txtCode").value;
+		nouvelleDevise.name = document.getElementById("txtName").value;
+		nouvelleDevise.change = document.getElementById("txtChange").value;
+		makeAjaxPostRequest("./devise-api/public/devise" , 
+				            JSON.stringify(nouvelleDevise), 
+				            cbGererResultatPost);
+	});
+}
+
+function cbGererResultatPost(texteReponse){
+	divMessage = document.getElementById("divMessage");
+	divMessage.innerHTML= texteReponse;
 }
 
 function cbGererResultat(texteReponse){
@@ -31,4 +52,16 @@ function makeAjaxGetRequest(url,callback) {
 	};
 	xhr.open("GET", url, true);
 	xhr.send(null);
+}
+
+function makeAjaxPostRequest(url,jsonData,callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+		    callback(xhr.responseText);
+		}
+	};
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.send(jsonData);
 }
