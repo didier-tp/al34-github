fa7e79b5f5aea487419d865ec91da0fb66c9a873
didier-tp <didier@d-d-efrance.fr>
2020-01-07 11:53:03 +0100

    s

--- springBootWs/src/main/resources/META-INF/resources/appelAjax.js
+++ springBootWs/src/main/resources/META-INF/resources/appelAjax.js
@@ -1,8 +1,10 @@
+var divRes ;
+
 window.onload=function(){
 	//alert("code declenché au chargement de la page");
 	var inputChangeMini = document.getElementById("txtChangeMini");
 	var btnGetDevises = document.getElementById("btnGetDevises");
-	var divRes = document.getElementById("divRes");
+	divRes = document.getElementById("divRes");
 	
 	btnGetDevises.addEventListener("click" , function (){
 		//alert("click sur bouton");
