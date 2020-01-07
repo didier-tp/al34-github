755fe8ca6500b6d457829fe5203de44f8e76f63b
didier-tp <didier@d-d-efrance.fr>
2020-01-07 11:49:44 +0100

    appel ajax

--- springBootWs/src/main/resources/META-INF/resources/appelAjax.js
+++ springBootWs/src/main/resources/META-INF/resources/appelAjax.js
@@ -7,6 +7,23 @@ window.onload=function(){
 	btnGetDevises.addEventListener("click" , function (){
 		//alert("click sur bouton");
 		var changeMini = inputChangeMini.value;
-		divRes.innerHTML="<b>"+ changeMini + "</b>";
+		//divRes.innerHTML="<b>"+ changeMini + "</b>";
+		makeAjaxGetRequest("./devise-api/public/devise?changeMini="+changeMini ,
+				           cbGererResultat);
 	});
 }
+
+function cbGererResultat(texteReponse){
+	divRes.innerHTML = texteReponse;
+}
+
+function makeAjaxGetRequest(url,callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+		callback(xhr.responseText);
+		}
+	};
+	xhr.open("GET", url, true);
+	xhr.send(null);
+	}
