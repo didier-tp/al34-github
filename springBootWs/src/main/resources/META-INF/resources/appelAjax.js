9fab63e70ba05339109b73b0a1722654d176a5ed
didier-tp <didier@d-d-efrance.fr>
2020-01-07 11:54:36 +0100

    s

--- springBootWs/src/main/resources/META-INF/resources/appelAjax.js
+++ springBootWs/src/main/resources/META-INF/resources/appelAjax.js
@@ -1,17 +1,12 @@
 var divRes ;
-
 window.onload=function(){
-	//alert("code declenché au chargement de la page");
 	var inputChangeMini = document.getElementById("txtChangeMini");
 	var btnGetDevises = document.getElementById("btnGetDevises");
 	divRes = document.getElementById("divRes");
 	
 	btnGetDevises.addEventListener("click" , function (){
-		//alert("click sur bouton");
 		var changeMini = inputChangeMini.value;
-		//divRes.innerHTML="<b>"+ changeMini + "</b>";
-		makeAjaxGetRequest("./devise-api/public/devise?changeMini="+changeMini ,
-				           cbGererResultat);
+		makeAjaxGetRequest("./devise-api/public/devise?changeMini="+changeMini ,  cbGererResultat);
 	});
 }
 
@@ -23,9 +18,9 @@ function makeAjaxGetRequest(url,callback) {
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
-		callback(xhr.responseText);
+		    callback(xhr.responseText);
 		}
 	};
 	xhr.open("GET", url, true);
 	xhr.send(null);
-	}
+}
