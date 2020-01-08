9d427ccd2ce3a0d860edbfe985161bd667b2a084
didier-tp <didier@d-d-efrance.fr>
2020-01-08 14:34:00 +0100

    s

--- springBootWs/src/main/java/fr/afcepf/al34/ws/rest/LoginRestCtrl.java
+++ springBootWs/src/main/java/fr/afcepf/al34/ws/rest/LoginRestCtrl.java
@@ -1,7 +1,11 @@
 package fr.afcepf.al34.ws.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,20 +18,32 @@ import fr.afcepf.al34.ws.dto.LoginResponse;
 @RequestMapping(value="/devise-api/public/login" , headers="Accept=application/json")
 public class LoginRestCtrl {
 	
+	
+	@Autowired
+    private AuthenticationManager authenticationManager;
+	
 
-	//http://localhost:8080/springBootWs/devise-api/public/login
+	//http://localhost:8383/springBootWs/devise-api/public/login
 	//avec { "username" : "user1" , "password" : "pwd1" }
 	//en retour { "message" :  "..." , "token" ; "..." , ...}
 	@PostMapping("")
 	public ResponseEntity<LoginResponse> postLogin(@RequestBody LoginRequest loginRequest){
 		LoginResponse loginResponse = new LoginResponse();
 		loginResponse.setUsername(loginRequest.getUsername());
-		if(loginRequest.getUsername().equals("user1") && 
-				loginRequest.getPassword().equals("pwd1")) {
+		/* if(loginRequest.getUsername().equals("user1") && 
+				loginRequest.getPassword().equals("pwd1")) */
+		try {
+		Authentication authentication = null;
+		//vérifier le username/password avec SpringSecurity:
+		authentication=authenticationManager.authenticate(
+		        new UsernamePasswordAuthenticationToken(
+		                loginRequest.getUsername(),
+		                loginRequest.getPassword()
+		        ));//authenticate() soulève une exception si mauvais username ou password
 			loginResponse.setOk(true);
 			loginResponse.setMessage("successful login");
 			loginResponse.setToken("jeton_qui_va_bien");
-		}else {
+		}catch(Exception ex) {
 			loginResponse.setOk(false);
 			loginResponse.setMessage("login failed");
 			return new ResponseEntity<LoginResponse>(loginResponse,HttpStatus.UNAUTHORIZED);
