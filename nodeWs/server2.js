59bf58658f2697914892048834326e4ee338dff1
didier-tp <didier@d-d-efrance.fr>
2020-01-09 15:12:25 +0100

    s

--- nodeWs/server2.js
+++ nodeWs/server2.js
@@ -29,6 +29,24 @@ app.get("/api-banque/tauxInteretCourant", function(req, res,next) {
 	res.send(objDataRes);
 });
 
+// GET http://localhost:8686/api-banque/fraisDossier?montant=28900
+app.get("/api-banque/fraisDossier", function(req, res,next) {
+	var montant = Number(req.query.montant);
+	var fraisDossier ;
+    if(montant < 25000) 
+		fraisDossier=100;
+	else if(montant >=25000 && montant < 100000)
+		fraisDossier=200;
+	else if(montant >=100000)
+		fraisDossier=300;
+	
+	var objDataRes = {
+		montant : montant,
+		fraisDossier : fraisDossier
+	};
+	res.send(objDataRes);
+});
+
 
 
 app.listen(8686,function () {
